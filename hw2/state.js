74e752ac8c77fa5b632fb4ec405c18533de5abbb
austin <as0983228754@gmail.com>
2023-04-04 17:28:36 +0800

    test touch event

--- hw2/state.js
+++ hw2/state.js
@@ -167,7 +167,7 @@ export class Context {
     }
     onTouchEnd(event) {
         var now = new Date().getTime();
-        console.log("touch end");
+        console.log("touch end", now, this.lastTouchTime);
         if (now - lastTouchTime <= 500) {
             var nowTarget = document.getElementsByClassName("target")[this.targetNumber];
             nowTarget.style.backgroundColor = 'green'
